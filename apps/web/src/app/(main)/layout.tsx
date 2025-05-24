68b16b9d7b6977a9e45d7901ca1764310d1bc636
Nathan Somto <mkparusomtochi26@gmail.com>
2025-05-24 08:06:25 +0100

    feat(apps/web/layout): add error handling for user settings retrieval and default settings fallback

--- apps/web/src/app/(main)/layout.tsx
+++ apps/web/src/app/(main)/layout.tsx
@@ -1,13 +1,20 @@
 import { SettingsProvider } from '#/features/settings/settings-provider';
 import { getUserSettings } from '#/services/settings';
+import { promiseCatch } from '#/utils/promise-catch';
+import { userSettingsDefaults } from '@designr/db/user-settings';
 import React from 'react'
 
 export default async function MainLayout({ children }: React.PropsWithChildren) {
-    const settings = await getUserSettings();
-    //console.log("the settings: ", settings);
+    const res = await promiseCatch(getUserSettings());
+    if (res?.type === 'error' && res.statusCode === 401) {
+        return <div
+            className='flex items-center justify-center h-screen w-screen text-destructive text-3xl font-semibold bg-destructive/10'>
+            Unauthorized</div>
+
+    }
     return (
         <SettingsProvider
-            initialSettings={settings}
+            initialSettings={res?.type === 'success' ? res.settings : userSettingsDefaults}
         >
             {
                 children
