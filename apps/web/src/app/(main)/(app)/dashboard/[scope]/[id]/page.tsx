79b2dee75d4ce6b17686d9746e1505e0c7fc7f2f
Nathan Somto <mkparusomtochi26@gmail.com>
2025-05-24 07:34:03 +0100

    feat(apps/web/dashboard): implement suspense loading for designs section with error handling

--- apps/web/src/app/(main)/(app)/dashboard/[scope]/[id]/page.tsx
+++ apps/web/src/app/(main)/(app)/dashboard/[scope]/[id]/page.tsx
@@ -1,16 +1,30 @@
 import DashboardBanner from '#/features/dashboard/banner'
 import EditorPresetsList from '#/features/dashboard/editor-preset-list'
 import DesignsSection from '#/features/dashboard/designs-section'
-import React from 'react'
+import React, { Suspense } from 'react'
+import { promiseCatch } from '#/utils/promise-catch'
+import { fetchProjects } from '#/services/projects'
+import { UserDesignsData } from '#/hooks/useProjects'
+import DesignsSectionLoader from '#/features/dashboard/designs-section-loader'
+import { PaginatedResponse } from '#/services'
 
 export default function DashboardPage() {
+    const fetchDesignsPromise = promiseCatch(fetchProjects('mine'))
     return (
         <>
             <section id="dashboard__home-section" className='min-h-screen px-6 py-2'>
                 <DashboardBanner />
                 <EditorPresetsList />
                 <div className='mt-14' />
-                <DesignsSection />
+                <Suspense
+                    fallback={
+                        <DesignsSectionLoader />
+                    }
+                >
+                    <DesignsSection
+                        fetchDesignsPromise={fetchDesignsPromise as Promise<PaginatedResponse<UserDesignsData>>}
+                    />
+                </Suspense>
             </section>
         </>
     )
