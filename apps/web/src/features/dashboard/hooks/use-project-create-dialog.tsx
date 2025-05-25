d80d6c59d30543d18d3ee7ffab7bc275b55ae737
Nathan Somto <mkparusomtochi26@gmail.com>
2025-05-25 13:12:17 +0100

    feat(apps/web/editor): add project creation to tool-bar and update time-stamp for untitled designs

--- apps/web/src/features/dashboard/hooks/use-project-create-dialog.tsx
+++ apps/web/src/features/dashboard/hooks/use-project-create-dialog.tsx
@@ -60,6 +60,9 @@ export function useCreateProjectDialog({
                     year: 'numeric',
                     month: '2-digit',
                     day: '2-digit',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit'
                 }).replace(/\//g, '-');
                 localName = `Untitled (${formattedDate})`;
             }
--- apps/web/src/features/editor/components/nav-bar/tool-bar/action-menu.tsx
+++ apps/web/src/features/editor/components/nav-bar/tool-bar/action-menu.tsx
@@ -1,19 +1,55 @@
 import { Button } from '@designr/ui/components/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuSeparator, DropdownMenuItem } from '@designr/ui/components/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuSeparator } from '@designr/ui/components/dropdown-menu'
 import { ChevronDownIcon } from 'lucide-react'
 import { actionMenuItems } from '../data'
-import React from 'react'
+import React, { useTransition } from 'react'
 import { useRouter } from 'next/navigation'
 import { LINKS } from '#/constants/links'
+import { createANewProject } from '#/services/projects'
+import { useSettings } from '#/features/settings/settings-provider'
+import { userSettingsDefaults } from '@designr/db/user-settings'
+import { promiseCatch } from '#/utils/promise-catch'
+import { toast } from 'sonner'
+import { handleProjectRedirect } from '#/utils/handle-project-redirect'
 type Action = typeof actionMenuItems[number]['action']
 export default function ActionMenu() {
     const router = useRouter();
-    const handleAction = (
+    const [isPending, startTransition] = useTransition()
+    const { settings } = useSettings();
+    const handleAction = async (
         action: Action
     ) => {
         switch (action) {
             case 'new':
-                // executes new file action
+                // executes new a project action
+                startTransition(async () => {
+                    const date = new Date();
+                    const formattedDate = date.toLocaleDateString(undefined, {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        second: '2-digit'
+                    }).replace(/\//g, '-');
+                    const res = await promiseCatch(createANewProject({
+                        height: settings?.defaultHeight ?? userSettingsDefaults?.defaultHeight,
+                        width: settings?.defaultWidth ?? userSettingsDefaults?.defaultWidth,
+                        name: `Untitled (${formattedDate})`
+                    }))
+                    if (res === undefined) {
+                        toast.error('Failed to Create project');
+                    }
+                    if (res?.type === 'error') {
+                        toast.error(res.message);
+                        return;
+                    }
+                    handleProjectRedirect({
+                        openInNewTab: settings?.openDesignsInNewTab ?? false,
+                        organizationId: res?.organizationId ?? '',
+                        projectId: res?.id ?? ''
+                    })
+                })
                 break;
             case 'dashboard':
                 router.push(LINKS.DASHBOARD)
@@ -34,6 +70,7 @@ export default function ActionMenu() {
                     variant={'selection'}
                     className='font-medium'
                     size='shrink'
+                    disabled={isPending}
                 >
                     D
                     {/* on hover the chevron should go down a bit */}
@@ -51,14 +88,15 @@ export default function ActionMenu() {
                 sideOffset={10}
                 align='end'
                 alignOffset={10}
-                className='ml-8'
+                className='ml-8 !z-[80] relative'
             >
                 {actionMenuItems.map(({ Icon, action, label }, index) => (
                     <React.Fragment key={action}>
                         <Button
+                            disabled={isPending}
                             variant={'selection'}
                             className='w-full justify-start text-xs font-medium'
-                            onClick={() => handleAction(action)}
+                            onClick={async () => await handleAction(action)}
                         >
                             <Icon />
                             {label}
